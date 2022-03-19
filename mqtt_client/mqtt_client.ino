2fb1f6f475d84f4e3b64ab6a1ffb7127f58a4ce8
Andrea Artioli <241745@studenti.unimore.it>
2022-03-19 22:51:34 +0100

    library changed

--- mqtt_client/mqtt_client.ino
+++ mqtt_client/mqtt_client.ino
@@ -13,77 +13,107 @@
 #endif
 
 #include <ArduinoOTA.h>
-#include <TinyMqtt.h>
+#include <AsyncMqttClient.h>
 
-static MqttClient* client;
-int N_CLIENT = 0;
+#define MAX_CLIENT 10
+int nclient = 0;
+AsyncMqttClient client[MAX_CLIENT];
+const short MQTT_PORT = 1883;
+
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+  Serial.println("Publish received.");
+  Serial.print("  topic: ");
+  Serial.println(topic);
+  Serial.print("  qos: ");
+  Serial.println(properties.qos);
+  Serial.print("  dup: ");
+  Serial.println(properties.dup);
+  Serial.print("  retain: ");
+  Serial.println(properties.retain);
+  Serial.print("  len: ");
+  Serial.println(len);
+  Serial.print("  index: ");
+  Serial.println(index);
+  Serial.print("  total: ");
+  Serial.println(total);
+}
 
 void setup() {
 #ifdef PORT
   ArduinoOTA.setPort(PORT);
 #endif
 
+  Serial.begin(115200);
+  while (!Serial);
   WiFi.mode(WIFI_STA);
 
-  //#ifdef NETWORK_NAME and NETWORK_PASSWORD
   WiFi.begin(STRINGIZE_VALUE_OF(NETWORK_NAME), STRINGIZE_VALUE_OF(NETWORK_PASSWORD));
 
-  Serial.begin(9600);
   while (!Serial);
 
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    //Serial.println("Connection Failed! Rebooting...");
+    Serial.println("Connection Failed! Rebooting...");
     delay(5000);
     ESP.restart();
   }
 
-#ifdef PASSWORD
-  // If you want authentication, uncomment this line
+#ifdef OTA_PASSWORD
   ArduinoOTA.setPassword(STRINGIZE_VALUE_OF(PASSWORD));
 #endif
 
 #ifdef HOSTNAME
-  ArduinoOTA.setHostname(STRINGIZE_VALUE_OF(HOSTNAME));
-  WiFi.setHostname(STRINGIZE_VALUE_OF(HOSTNAME));
-  MDNS.begin(STRINGIZE_VALUE_OF(HOSTNAME));
+  const char* hostname = String(STRINGIZE_VALUE_OF(HOSTNAME)).c_str();
 #else
-  ArduinoOTA.setHostname("ESP_OTA");
-  WiFi.setHostname("ESP_OTA");
-  MDNS.begin("ESP_OTA");
+  const char* hostname = "ESP-MQTT-CLIENT-" + String(random(100)).c_str();
 #endif
 
+  ArduinoOTA.setHostname(hostname);
+  WiFi.setHostname(hostname);
   ArduinoOTA.begin();
-  
-  int n = MDNS.queryService("mqtt", "tcp");
 
-  if (n > 0) {
-    N_CLIENT = n;
-    client = (MqttClient*) malloc(sizeof(MqttClient) * n);
-    for (int i = 0; i < N_CLIENT; ++i) {
-      //MDNS.IP(i);
-      client[i].connect(MDNS.hostname(i).c_str(), (uint16_t)1883);  
+  nclient = MDNS.queryService("_mqtt", "_tcp");
+ 
+  Serial.println("Ho fatto la query");
+
+  if (nclient > 0) {
+    Serial.println("Ho trovato qualcosa con la query");
+    //MDNS.IP(i);
+
+    for( int i = 0; i < nclient; i++){
+      client[i].setServer(MDNS.IP(i), MQTT_PORT);
+      client[i].onMessage(onMqttMessage);
+      client[i].connect();
+      client[i].subscribe("prova/test", 0);
+
+      Serial.println("Ho trovato: " + MDNS.hostname(i));
     }
-
   } else {
 
     ESP.restart();
   }
 
-  
-
+  Serial.println("Ho finito il setup");
 }
 
 void loop() {
-  
-  for (int i = 0; i < N_CLIENT; ++i) {
-      client[i].loop(); 
-  }
-  
   ArduinoOTA.handle();
-  delay(500);
-  auto rnd=random(100);
-  for (int i = 0; i < N_CLIENT; ++i) {
-      client[i].publish("prova/test", String(rnd));
-      
+
+  Serial.println("Sono nel loop");
+
+  Serial.println("Sono nel loop, sto per entrare nel for");
+  for (int i = 0; i < nclient; ++i) {
+    
+    auto rnd = random(100);
+    Serial.println("Client " + String(i) + ": sto provando a mandare qualcosa: " + String(rnd));
+    client[i].connect();
+    auto timestart = millis();
+    while(!client[i].connected()){
+      if (millis() - timestart > 1000)
+        break;
+    }
+    client[i].publish("prova/test", 0, false, String(rnd).c_str());
+
   }
+  
+  delay(500);
 }
